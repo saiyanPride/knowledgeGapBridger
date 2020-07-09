bc943f61b3b517696bcff3b76ad0c72eaa246655
saiyanPride <none@example.com>
2020-07-09 08:09:42 +0100

    implemented version 1 of algo for summarising search engine query result snippets (not tested)

--- src/queryResultAnalyser/queryResultAnalyser.py
+++ src/queryResultAnalyser/queryResultAnalyser.py
@@ -1,40 +1,67 @@
 def get_nearest_keyword_distance(cnk_start, cnk_end, keyword_ranks):
-  """
-  `keywordRanks` has to be non-empty list!; keywordRanks[i] is the rank of keyword i within the snippet. 
-  Rank of a word refers to its order in the snippet string, hint: 1st word in the snippet has a rank of 1, 2nd word has a rank of 2 and so on
-  There is an implicit assumption, that the snippet would only be a few words i.e. not billions of words, otherwise integer overflows may occur when dealing with ranks e.g. the 5 billionth word's rank can't be stored in 32 bit int
-  """
-  assert(len(keyword_ranks) >= 1)
-  assert(cnk_start <= cnk_end)
-  # if(start == end), cnk is a single word
-    # cnk_rank = start
-    # handle case for 1 keyword => minDist = abs(keyword_ranks[0]- cnk_rank); 
-    # handle case for at least 2 keywords
-    # if rank(cnk) < rank(firstKeyword) nearestKeyword => firstKeyword
-    # elif rank(wordi) > rank(lastKeyword) nearestKeyword => lastKeyword
-    # else: nonkeywordi is enclosed by 2 keywords, determine the nearest one to left & right, then find minimum distance
-      #minDistRight: iterate from leftmostkeyword and find first rank larger than `cnkRank`, we know one exists, then find distance
-      #minDistLeft: lbKeyword is the one just before the upper bound keyword
-      #minDistance = min(minDistLeft,minDistRight)
-      #.nearestLeftKeyWord...cnk...nearestRightKeyword
-  # if(start < end), cnk has at least 2 words
-    # handle case for 1 keyword
-      #keyword is either to the left or the right of cnk
-      # if keywordRank < cnkStart, minDist = cnkStart-keywordRank
-      # else minDist = keywordRank - cnkEnd
-    # handle case for >=2 keywordRanks
-      # ...firstKeyword...lastKeyWord...; where is cnk located out of the 3 regions
-      # if (cnkEnd < rank(firstKeyword)) ; cnk is in region before first keyword
-        #nearestKeyword => firstKeyword, minDist = rank(firstKeyword) -cnkEnd
-      # if (cnkStart > rank(lastKeyword), minDist = cnkStart - rank(lastKeyword)
-      # else we know we have this case: ...firstKeyword...cnk...lastKeyWord...
-      # this means from cnk's perspective we have
-      # nearestLeftKeyWord...cnkStart->cnkEnd...nearestRightKeyword, the gaps in this case contain 0 or more words e.g. 
-        #iterate through the keyword ranks, and find first rank larger than cnkEnd => minDistRight = rank(nearestRightKeyword) -cnkEnd
-       # minDistLeft = cnkStart - rank(nearestLeftKeyWord), where rank(nearestLeftKeyWord) is the rank of keyword to the immediate left of nearestRightKeyword
-        # minDist = min(minDistLeft,minDistRight) 
-  pass
+    """
+    keyword_ranks has to be non-empty list!
+    keyword_ranks[i] is the rank of keyword i within the query result snippet. 
+
+    cnk refers to a consecutive non-keyword: it could be a single word or a collection of words(phrase) which have been deemed noteworthy, 
+    thus it will be included in the summary of the snippet being processed
+    
+    Rank of a word refers to its order in the snippet string, so the ith word in the snippet has a rank of i where i=1,2,3,...
+    There is an implicit assumption, that the snippet will only be a few words i.e. not billions of words, otherwise integer overflows may occur when dealing with ranks e.g. the 5 billionth word's rank can't be stored in a 32 bit int
+    """
+    keyword_size = len(keyword_ranks)
+    assert(keyword_size) >= 1)
+    assert(cnk_start <= cnk_end)
+    result = 0
+
+    def get_nearest_right_keyword_for_enclosed_cnk(cnk_rank): #nested
+        """
+        cnk is guaranteed to be surrounded by keywords i.e. it has a nearest left keyword
+        """
+        nearest_right_keyword_index = -1
+            for i in range(0,keyword_size): # find index in keyword_ranks corresponding to nearestRightKeyword 
+                if keyword_ranks[i] > cnk_rank: 
+                    nearest_right_keyword_index = i
+        assert(nearest_right_keyword_index != -1)# this assertion is redundant, it is guaranteed that the nearest_right_keyword_index was found and set
+        return nearest_right_keyword_index
+        
+
+    if cnk_start == cnk_end: # cnk is a single word
+        cnk_rank = cnk_start
+        
+        if keyword_size == 1: # handle case for 1 keyword
+            result = abs(keyword_ranks[0]- cnk_rank)
+        elif cnk_rank < keyword_ranks[0]: # cnk is to the left of first keyword i.e. cnk...firstKeyword
+            result = keyword_ranks[0]- cnk_rank
+        elif cnk_rank > keyword_ranks[keyword_size-1]: # cnk is to the right of last keyword i.e. lastKeyword...cnk
+            result = cnk_rank - keyword_ranks[keyword_size-1]
+        else: # cnk enclosed by 2 keywords on either side i.e. nearestLeftKeyWord...cnk...nearestRightKeyword
+            # find rank of nearestRightKeyword, and use it to determine rank of nearestLeftKeyWord
+            nearest_right_keyword_index = get_nearest_right_keyword_for_enclosed_cnk(cnk_rank)
+            nearest_left_keyword_index = nearest_right_keyword_index - 1
+            result = min(keyword_ranks[nearest_right_keyword_index] - cnk_rank, cnk_rank - keyword_ranks[nearest_left_keyword_index])
+    else: # cnk consists of >=2 consective words i.e. cnk_start < cnk_end
+        if keyword_size == 1: # the sole keyword is either to the left or right of the cnk
+            result = cnk_start - keyword_ranks[0] if keyword_ranks[0] < cnk_start else keyword_ranks[0] - cnk_end
+        elif cnk_end < keyword_ranks[0]: # keyword_size is guaranteed to be >=2 since if this line reached i.e. we have this snippet general form 
+                                            # ...firstKeyword...lastKeyWord...), so handle cases where is cnk located in each of the 3 regions (...)
+            result = keyword_ranks[0] - cnk_end # nearestKeyword => firstKeyword so cnk is in region before first keyword
+        elif cnk_start > keyword_ranks[keyword_size-1]:
+            result = cnk_start - keyword_ranks[keyword_size-1] # since cnk is in region after last keyword
+        else:   # cnk is located between the first & last keywords i.e. ...firstKeyword...cnk...lastKeyWord...
+            # format of region near cnk will look like: nearestLeftKeyWord...cnkStart->cnkEnd...nearestRightKeyword
+            # find index of nearestRightKeyword, then deduce index of nearestLeftKeyWord within keyword_ranks, determine their respective ranks and minimum distance of cnk to its nearest keyword
+            nearest_right_keyword_index = get_nearest_right_keyword_for_enclosed_cnk(cnk_end)
+            nearest_left_keyword_index = nearest_right_keyword_index - 1
+            min_keyword_dist_to_right = keyword_ranks[nearest_right_keyword_index] - cnk_end
+            min_keyword_dist_to_left = cnk_start - keyword_ranks[nearest_left_keyword_index]
+            result = min(min_keyword_dist_to_right, min_keyword_dist_to_left)
+    return result
+
 def summarise_snippet(snippet, keywords_set, is_noteworthy_word):
+    """
+    is_noteworthy_word is a function which examines a word and determines whether it is noteworthy or not, noteworthy words will be present in the summary that will be returned
+    """
     assert(len(snippet)>=1)
     # store the index of all keywords in an array
     
